fc04c68d79fcf0113438e0c304230b2f74151735
Deagel1337 <yehor1@freenet.de>
2024-04-22 18:47:02 +0200

    Added draw trait

--- rust-console-shaper/src/line.rs
+++ rust-console-shaper/src/line.rs
@@ -1,3 +1,5 @@
+use crate::draw::Draw;
+
 #[derive(Debug)]
 pub struct Line {
     x: i32,
@@ -14,6 +16,16 @@ impl Line {
 
     pub fn coordinates(&self) -> (i32,i32){
         (self.x,self.y)
+    }
+
+    pub fn length(&self) -> f32 {
+        f32::sqrt((i32::pow(self.x,2) as f32)-(i32::pow(self.y,2) as f32))
+    }
+}
+
+impl Draw for Line {
+    fn draw(&self) -> String {
+        let str: String = String::new();
     }
 }
 
